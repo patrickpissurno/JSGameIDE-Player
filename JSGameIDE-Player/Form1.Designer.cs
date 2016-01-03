6f0bc5e7fa576a2eed653c230c3cb12aa88c77ec
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-01-03 15:15:59 -0200

    Added some event handlers
    - Hidden the loading
    - Form title now changes according to "Loading" and loaded page title
    - Overflow="Hidden" auto injector

--- JSGameIDE-Player/Form1.Designer.cs
+++ JSGameIDE-Player/Form1.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.browserPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // browserPanel
+            // 
+            this.browserPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserPanel.Location = new System.Drawing.Point(0, 0);
+            this.browserPanel.Name = "browserPanel";
+            this.browserPanel.Size = new System.Drawing.Size(784, 561);
+            this.browserPanel.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.browserPanel);
             this.Name = "Form1";
-            this.Text = "JSGameIDE Player";
+            this.Text = "Loading...";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel browserPanel;
     }
 }
 
--- JSGameIDE-Player/Form1.cs
+++ JSGameIDE-Player/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
 
 namespace JSGameIDE_Player
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser Browser;
         public Form1()
         {
             InitializeComponent();
+            Init();
         }
+
+        public void Init()
+        {
+            Cef.Initialize(new CefSettings());
+            Browser = new ChromiumWebBrowser("www.google.com");
+            Browser.Dock = DockStyle.Fill;
+            Browser.LoadingStateChanged += Browser_LoadingStateChanged;
+            Browser.TitleChanged += Browser_TitleChanged;
+            browserPanel.Controls.Add(Browser);
+        }
+
+        private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            SetTitle(e.Title);
+        }
+
+        private void Browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            Browser.EvaluateScriptAsync("document.getElementsByTagName('body')[0].style.overflow = 'hidden'");
+            ToggleBrowser(!e.IsLoading);
+            if (e.IsLoading)
+                SetTitle("Loading...");
+        }
+
+        public void SetTitle(string str)
+        {
+            if(this.InvokeRequired)
+                this.Invoke(new MethodInvoker(() => { SetTitle(str); }));
+            else
+            {
+                this.Text = str;
+            }
+        }
+
+        public void ToggleBrowser(bool Visible)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(() => { ToggleBrowser(Visible); }));
+            else
+            {
+                if(Visible)
+                    browserPanel.Show();
+                else
+                    browserPanel.Hide();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
     }
 }
